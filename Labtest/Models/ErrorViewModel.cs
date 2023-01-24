9f0cdfd06cc66d85d9270a2f2de245d0cf1e41c6
mohit97289 <mohitsinghdhanda1997@gmail.com>
2023-01-24 14:53:21 -0500

    scaffolded project

---
+++ Labtest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Labtest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
